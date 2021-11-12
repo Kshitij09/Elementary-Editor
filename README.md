6a848a2d8ebabae7384503b1a2e969062a9f67fe
Kshitij Patil <kshitijpatil98@gmail.com>
2021-11-13 00:32:24 +0530

    Initialize Android Project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Elementary-Editor
+
+> A basic Image Editor implemented as part of an assignment given by Totality Corp
---
+++ app/src/main/java/com/kshitijpatil/elementaryeditor/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.kshitijpatil.elementaryeditor
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
