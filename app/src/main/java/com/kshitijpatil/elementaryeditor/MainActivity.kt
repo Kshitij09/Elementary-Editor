832d15b2fd70374c0036e1bd3e2331de8317f8ab
Kshitij Patil <kshitijpatil98@gmail.com>
2021-11-17 00:33:17 +0530

    Prepare MainActivity Layout
    Signed-off-by: Kshitij Patil <kshitijpatil98@gmail.com>

--- app/src/main/java/com/kshitijpatil/elementaryeditor/MainActivity.kt
+++ app/src/main/java/com/kshitijpatil/elementaryeditor/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.kshitijpatil.elementaryeditor
 
 import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<FrameLayout>(R.id.root_container).setOnClickListener {
+            Timber.d("\'Anywhere\' clicked")
+        }
+        findViewById<ImageView>(R.id.iv_launch_camera).setOnClickListener {
+            Timber.d("Camera Button clicked")
+        }
     }
 }
